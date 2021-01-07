2dd3b8939a13b78159794507d889554c87f1d51b
WordWei <861064676@qq.com>
2021-01-07 15:38:13 +0800

    001,两数之和

---
+++ src/001、两数之和/Solution.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+/**
+ * @author: liwei
+ * @create: 2021/1/7 0007
+ * @description:
+ **/
+
+/**
+ * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 的那两个整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ *
+ */
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                return new int[]{hashMap.get(nums[i]), i};
+            }
+            hashMap.put(target - nums[i], i);
+        }
+        return null;
+    }
+}
