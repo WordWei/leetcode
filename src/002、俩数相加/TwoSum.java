42f89645e69ef09f8de8fe0f91515a4fd00638b3
WordWei <861064676@qq.com>
2021-01-12 16:37:01 +0800

    刷题

---
+++ src/002、俩数相加/TwoSum.java
@@ -0,0 +1,63 @@
+/**
+ * @author: liwei
+ * @create: 2021/1/12 0012
+ * @description:
+ **/
+
+
+/**
+ *
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ *
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ *
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ *
+ *
+ *
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ *
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
+ */
+
+
+public class TwoSum {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        int addOne = 0;
+        ListNode listNode = new ListNode(0);
+        ListNode temp = listNode;
+        while ((addOne != 0) || l1 !=null || l2 != null) {
+            int a = l1 != null ? l1.val : 0;
+            int b = l2 != null ? l2.val : 0;
+            int res = (a + b + addOne) % 10;
+            addOne = (a + b + addOne) >= 10 ? 1 : 0;
+            temp.next = new ListNode(res);
+            temp = temp.next;
+            if (l1 != null) {l1 = l1.next;}
+            if (l2 != null) {l2 = l2.next;}
+        }
+        return listNode.next;
+    }
+}
