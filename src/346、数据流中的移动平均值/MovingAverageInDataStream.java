c0944b82263f49b09c5722113b07019c085c9517
WordWei <861064676@qq.com>
2021-01-19 11:28:59 +0800

    leetcode 2021-01-19

---
+++ src/346、数据流中的移动平均值/MovingAverageInDataStream.java
@@ -0,0 +1,19 @@
+/**
+ * @author: liwei
+ * @create: 2021/1/19 0019
+ * @description:
+ **/
+
+/**
+
+ *
+ * 给定一个整数数据流和一个窗口大小，根据该滑动窗口的大小，计算其所有整数的移动平均值。
+ * MovingAverage m = new MovingAverage(3);
+ * m.next(1) = 1
+ * m.next(10) = (1 + 10) / 2
+ * m.next(3) = (1 + 10 + 3) / 3
+ * m.next(5) = (10 + 3 + 5) / 3
+ *
+ */
+public class MovingAverageInDataStream {
+}
---
+++ src/35、搜索插入位置/SearchInsertPosition.java
@@ -0,0 +1,54 @@
+/**
+ * @author: liwei
+ * @create: 2021/1/19 0019
+ * @description:
+ **/
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ */
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+
+        int n = nums.length;
+        int left = 0, right = n -1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1)  + left;
+            if (nums[mid] >= target) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+
+    }
+    public int searchInsert1(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    
+
+}
